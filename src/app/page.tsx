d699c1ae58649949312c52b65872176b36432ffe
Wenny Chang <shine5983@gmail.com>
2024-06-15 23:53:22 +0800

    feat: update name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "@/app/ui/home/Navbar";
 export default function Home() {
   return (
     <main>
--- src/app/ui/common/Navbar.tsx
+++ src/app/ui/common/Navbar.tsx
@@ -54,7 +54,7 @@ function Navbar() {
               textDecoration: "none",
             }}
           >
-            RAINY
+            MYSECY
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -112,7 +112,7 @@ function Navbar() {
               textDecoration: "none",
             }}
           >
-            RAINY
+            MYSECY
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
